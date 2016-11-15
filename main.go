2bea7f87c67e671b8eee1f1a306d9ab3ad4bf598
Colin L. Rice <colin@daedrum.net>
2016-11-15 00:01:44 -0500

    Make it work

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"log"
+	"net"
+    "io"
+
+	"golang.org/x/net/proxy"
+)
+
+func main() {
+
+	d, err := proxy.SOCKS5("tcp", "localhost:9050", nil, &net.Dialer{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l, err := net.Listen("tcp", ":47854")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		tc, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+        log.Print("accepted")
+
+        sc, err := d.Dial("tcp", "2k3ffkhtbvxromr3.onion:22")
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go io.Copy(tc, sc)
+		go io.Copy(sc, tc)
+        log.Print("copying")
+
+	}
+
+}
